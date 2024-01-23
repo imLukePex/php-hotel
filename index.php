df092594391d8ad5962675483e7e8d97c56be31d
imLukePex <luca.pessione@gmail.com>
2024-01-23 19:08:32 +0100

    added ForEach Hotels

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>PHP Hotel</title>
+</head>
+<body>
+
+    <h1 style="text-align: center; ">List Hotels</h1>
+
+    <?php
+
+        $hotels = [
+
+            [
+                'name' => 'Hotel Belvedere',
+                'description' => 'Hotel Belvedere Descrizione',
+                'parking' => true,
+                'vote' => 4,
+                'distance_to_center' => 10.4
+            ],
+            [
+                'name' => 'Hotel Futuro',
+                'description' => 'Hotel Futuro Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 2
+            ],
+            [
+                'name' => 'Hotel Rivamare',
+                'description' => 'Hotel Rivamare Descrizione',
+                'parking' => false,
+                'vote' => 1,
+                'distance_to_center' => 1
+            ],
+            [
+                'name' => 'Hotel Bellavista',
+                'description' => 'Hotel Bellavista Descrizione',
+                'parking' => false,
+                'vote' => 5,
+                'distance_to_center' => 5.5
+            ],
+            [
+                'name' => 'Hotel Milano',
+                'description' => 'Hotel Milano Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 50
+            ],
+        
+        ];
+
+    ?>
+
+    <!-- Table -->
+    <div class="container">
+        <table class="table table-dark table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Parking</th>
+                    <th scope="col">Vote</th>
+                    <th scope="col">Distance to center</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+                foreach($hotels as $hotel){
+                    echo "<tr>";
+                        echo "<td>{$hotel['name']}</td>";
+                        echo "<td>{$hotel['description']}</td>";
+                        echo "<td>" . ($hotel['parking'] ? 'Yes' : 'No') . "</td>";
+                        echo "<td>{$hotel['vote']}</td>";
+                        echo "<td>{$hotel['distance_to_center']} km</td>";
+                    echo "</tr>";
+                }
+            ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
